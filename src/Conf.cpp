6b4960fd6282c31312ce9371559f49949062c8b7
Robert Alm Nilsson <rorialni@gmail.com>
2015-02-27 23:13:53 +0100

    Update year.

--- src/Conf.cpp
+++ src/Conf.cpp
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2014  Robert Alm Nilsson <rorialni@gmail.com>
+  Copyright (C) 2014, 2015  Robert Alm Nilsson <rorialni@gmail.com>
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
